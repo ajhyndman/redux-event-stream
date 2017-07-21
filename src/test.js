7089729d618dca14f42b2e79c28fe277be20c48a
Andrew Hyndman <ajhyndman@hotmail.com>
2017-07-21 21:13:05 +1000

    Swap tape for jest

--- src/test.js
+++ src/test.js
@@ -1,29 +1,19 @@
 // @flow
-import test from 'tape';
-
+/* eslint-env jest */
 import { createEventStream, createProjection } from './index.js';
 
-test('createEventStream', assert => {
-  assert.test('produces an object with a subscribe method', assert => {
-    assert.equal(typeof createEventStream({}).subscribe, 'function');
-    assert.end();
+describe('createEventStream', () => {
+  test('produces an object with a subscribe method', () => {
+    expect(typeof createEventStream({}).subscribe).toBe('function');
   });
-
-  assert.end();
 });
 
-test('createProjection', assert => {
-  assert.test(
-    'produces an object with getState and subscribe methods',
-    assert => {
-      const eventStream = createEventStream({});
-      const store = createProjection(eventStream, a => a);
+describe('createProjection', () => {
+  test('produces an object with getState and subscribe methods', () => {
+    const eventStream = createEventStream({});
+    const store = createProjection(eventStream, a => a);
 
-      assert.equal(typeof store.getState, 'function');
-      assert.equal(typeof store.subscribe, 'function');
-      assert.end();
-    },
-  );
-
-  assert.end();
+    expect(typeof store.getState).toBe('function');
+    expect(typeof store.subscribe).toBe('function');
+  });
 });
