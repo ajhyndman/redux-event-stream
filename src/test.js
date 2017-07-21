058243668b90254937190b6247d44c3541189a10
Andrew Hyndman <ajhyndman@hotmail.com>
2017-07-21 21:03:37 +1000

    Complete basic tests

--- src/test.js
+++ src/test.js
@@ -1,3 +1,4 @@
+// @flow
 import test from 'tape';
 
 import { createEventStream, createProjection } from './index.js';
@@ -7,5 +8,22 @@ test('createEventStream', assert => {
     assert.equal(typeof createEventStream({}).subscribe, 'function');
     assert.end();
   });
+
+  assert.end();
+});
+
+test('createProjection', assert => {
+  assert.test(
+    'produces an object with getState and subscribe methods',
+    assert => {
+      const eventStream = createEventStream({});
+      const store = createProjection(eventStream, a => a);
+
+      assert.equal(typeof store.getState, 'function');
+      assert.equal(typeof store.subscribe, 'function');
+      assert.end();
+    },
+  );
+
   assert.end();
 });
