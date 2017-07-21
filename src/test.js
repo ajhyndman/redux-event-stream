f4e31af67ec1f5b1c41479ccbef905c0bb8c1e6e
Andrew Hyndman <ajhyndman@hotmail.com>
2017-07-21 21:43:57 +1000

    Add behavioural tests for createEventStream

--- src/test.js
+++ src/test.js
@@ -6,6 +6,26 @@ describe('createEventStream', () => {
   test('produces an object with a subscribe method', () => {
     expect(typeof createEventStream({}).subscribe).toBe('function');
   });
+
+  test('produces an object with the command methods defined', () => {
+    const eventStream = createEventStream({
+      increment: () => ({ type: 'INCREMENT' }),
+    });
+    expect(typeof eventStream.increment).toBe('function');
+    expect(typeof eventStream.subscribe).toBe('function');
+  });
+
+  test('notifies subscribers whenever a command is dispatched', () => {
+    const eventStream = createEventStream({
+      increment: () => ({ type: 'INCREMENT' }),
+    });
+
+    const subscriber = jest.fn();
+    eventStream.subscribe(subscriber);
+    eventStream.increment();
+
+    expect(subscriber).toHaveBeenCalledTimes(1);
+  });
 });
 
 describe('createProjection', () => {
